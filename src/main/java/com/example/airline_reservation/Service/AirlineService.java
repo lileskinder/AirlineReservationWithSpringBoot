4473f9feff9bab50dfcb924eb2e188f19cee34fd
abdebra <68812780+abdebra@users.noreply.github.com>
2021-08-10 14:36:16 -0500

    Update AirlineService.java

--- src/main/java/com/example/airline_reservation/Service/AirlineService.java
+++ src/main/java/com/example/airline_reservation/Service/AirlineService.java
@@ -4,6 +4,7 @@ import com.example.airline_reservation.Model.Airline;
 import com.example.airline_reservation.Web.DTOs.AirlineDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AirlineService {
     public Airline findById(Integer id);
@@ -11,6 +12,6 @@ public interface AirlineService {
     public AirlineDTO Update(int id, AirlineDTO airlinDTO);
     public void delete(int id);
 
-    public List<AirlineDTO> findAll();
+    public List<AirlineDTO> findAll(Optional<Integer> page);
     public AirlineDTO save(AirlineDTO airlineDTO);
 }
