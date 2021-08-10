373c1e1c70c7808839da5a3eb12d616b17cf0291
Bayarbat Batbayar <b.bayarbat@gmail.com>
2021-08-09 23:21:59 -0500

    removed getter setter method from PersonDTO

--- src/main/java/com/example/airline_reservation/Service/DTOs/PersonDTO.java
+++ src/main/java/com/example/airline_reservation/Service/DTOs/PersonDTO.java
@@ -3,8 +3,6 @@ package com.example.airline_reservation.Service.DTOs;
 import com.example.airline_reservation.Model.Address;
 import lombok.Data;
 
-import javax.persistence.Embedded;
-
 @Data
 public class PersonDTO {
 	private int id;
@@ -15,46 +13,6 @@ public class PersonDTO {
 	private Address address;
 
 	public PersonDTO() {
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
 	}
 
 	public PersonDTO(String firstName, String lastName, String email, Address address) {
