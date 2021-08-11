500614cd898dd7366958d2ca8916c30f97be79cf
lileskinder <33198174+lileskinder@users.noreply.github.com>
2021-08-10 22:57:55 -0500

    Updated TicketController

--- src/main/java/com/example/airline_reservation/Web/TicketController.java
+++ src/main/java/com/example/airline_reservation/Web/TicketController.java
@@ -1,14 +1,13 @@
 package com.example.airline_reservation.Web;
 
-import com.example.airline_reservation.Model.Ticket;
-import com.example.airline_reservation.Service.DTOs.DTOAdapters.TicketDTOAdapter;
-import com.example.airline_reservation.Service.DTOs.PassengerTicketDTO;
-import com.example.airline_reservation.Service.DTOs.TicketDTO;
 import com.example.airline_reservation.Service.Implementation.TicketServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tickets")
@@ -22,6 +21,6 @@ public class TicketController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getTicket(@PathVariable int id) {
-        return new ResponseEntity<PassengerTicketDTO>(ticketService.getPassengerTicketDetails(id), HttpStatus.OK);
+        return new ResponseEntity<>(ticketService.getPassengerTicketDetails(id), HttpStatus.OK);
     }
 }
